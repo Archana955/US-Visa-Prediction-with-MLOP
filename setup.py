bcc0edb71a599210cba2d814acad4bcc11875cb8
Archana Parmar <archana.mandloi9@gmail.com>
2025-02-10 23:01:27 +0530

    requirments added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Archana",
+    author_email="archana.mandloi9@gmail.com",
+    packages=find_packages()
+)
